c42a391d2374de76ff9fd65d710033503dfd40f7
ISAIAS <issuyep@yahoo.com>
2023-06-26 12:29:45 -0600

    complete lab

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ function Scooter(year, color, model) {
     this.color = color;
     this.model = model;
 }
-var scooters1 = new scooters( 2015, "red", "swoosh");
+let scooters1 = new scooters( 2015, "red", "swoosh");
 
 function Driver(name, age, experience) {
     this.name = name;
     this.age = age;
     this.experience = experience;
 }
-var drivers1 = new drivers( "Allison", "16 years", "2 years");
+let drivers1 = new drivers( "Allison", "16 years", "2 years");
 
 function PickupLocation(address, city) {
     this.address = address;
     this.city = city;
 }
-var pickupLocation1 = new PickupLocation( "123 Broadway", "New York City");
+let pickupLocation1 = new PickupLocation( "123 Broadway", "New York City");
